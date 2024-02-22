398b182e1824e0e8382b8a100e40b2901ff2c922
stewartweiss <sweiss@hunter.cuny.edu>
2024-02-22 13:34:22 -0500

    Revised get_int.c

--- common/get_int.c
+++ common/get_int.c
@@ -21,14 +21,10 @@ int  get_int(char *arg, int flags, int *value, char *msg )
     int res =  get_long(arg, flags, &val, msg );
 
     if ( VALID_NUMBER == res ) {
-        if ( val > INT_MAX ) {
+        if ( val > INT_MAX  || val < INT_MIN ) {
             sprintf(msg, "%ld is out of range\n", val);
             return OUT_OF_RANGE;
         }
-        else if ( val < INT_MIN ) {
-            sprintf(msg, "%ld is out of range\n", val);
-            return OUT_OF_RANGE;
-            }
         else {
             *value = val;
             return VALID_NUMBER;
