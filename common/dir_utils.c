9dfbc9e60f0e049e8244a5ce8c6de843803a43b2
stewartweiss <sweiss@hunter.cuny.edu>
2024-11-18 11:49:45 -0500

    Changed style of isdir() (thanks to Mitch)

--- common/dir_utils.c
+++ common/dir_utils.c
@@ -15,15 +15,12 @@
 BOOL isdir(  const struct dirent *direntp)
 {
 #ifdef _DIRENT_HAVE_D_TYPE
-    if( direntp->d_type  == DT_DIR)
+    return ( direntp->d_type  == DT_DIR );
 #else
     struct stat    statbuf;
     stat(direntp->d_name, &statbuf);
-    if ( S_ISDIR(statbuf.st_mode) )
+    return ( S_ISDIR(statbuf.st_mode) );
 #endif
-        return TRUE;
-    else
-        return FALSE;
 }
 
 
