d484608dd29a2880709be6e747e56a9af39d3bb8
stewartweiss <sweiss@hunter.cuny.edu>
2026-01-01 12:43:05 -0500

    Modified chapter03/testspl_date3.sh to fix Issue #6

--- chapter03/testspl_date3.sh
+++ chapter03/testspl_date3.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-echo showdate3 -h ; showdate3 -h
-echo showdate3 +"%a %b %d, %Y, at %R" ; showdate3 +"%a %b %d, %Y, at %R"
-echo showdate3 -d '1 year'  +"%a %b %d, %Y, at %R" ; showdate3 -d '1 year'  +"%a %b %d, %Y, at %R"
-echo showdate3 -d '1 week 2 hours'  +"%a %b %d, %Y, at %R" ; showdate3 -d '1 week 2 hours'  +"%a %b %d, %Y, at %R"
-echo showdate3 -d '-2 months   +4 months'  +"%a %b %d, %Y, at %R" ; showdate3 -d '-2 months   +4 months'  +"%a %b %d, %Y, at %R"
-echo showdate3 -d ' +120 minutes -2 hours '  +"%a %b %d, %Y, at %R" ; showdate3 -d ' +120 minutes -2 hours '  +"%a %b %d, %Y, at %R"
+if [ ! -x testspl_date3 ] ; then
+    echo "Make sure the executable, spl_date3, exists before running this script."
+    exit
+fi
+
+echo testspl_date3 -h ; testspl_date3 -h
+echo testspl_date3 +"%a %b %d, %Y, at %R" ; testspl_date3 +"%a %b %d, %Y, at %R"
+echo testspl_date3 -d '1 year'  +"%a %b %d, %Y, at %R" ; testspl_date3 -d '1 year'  +"%a %b %d, %Y, at %R"
+echo testspl_date3 -d '1 week 2 hours'  +"%a %b %d, %Y, at %R" ; testspl_date3 -d '1 week 2 hours'  +"%a %b %d, %Y, at %R"
+echo testspl_date3 -d '-2 months   +4 months'  +"%a %b %d, %Y, at %R" ; testspl_date3 -d '-2 months   +4 months'  +"%a %b %d, %Y, at %R"
+echo testspl_date3 -d ' +120 minutes -2 hours '  +"%a %b %d, %Y, at %R" ; testspl_date3 -d ' +120 minutes -2 hours '  +"%a %b %d, %Y, at %R"
